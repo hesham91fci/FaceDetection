509817cceb0f68b905fab2feb59d3f49c8d51610
Hesham Ali <hesham_fci@yahoo.com>
2019-11-17 12:37:39 +0200

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Hesham Ali on 11/17/19.
+//  Copyright © 2019 Hesham Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
